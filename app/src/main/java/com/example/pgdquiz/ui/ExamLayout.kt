a6b0736f1eb43fa410720a552ca9b7dfb95d4f81
Thomas <Thomas.watson555@Gmail.com>
2024-10-08 07:05:40 +1300

    layout Trial

--- app/src/main/java/com/example/pgdquiz/ui/ExamLayout.kt
+++ app/src/main/java/com/example/pgdquiz/ui/ExamLayout.kt
@@ -1,10 +1,27 @@
 package com.example.pgdquiz.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.example.pgdquiz.R
 import com.example.pgdquiz.ui.Composables.Banner
+import com.example.pgdquiz.ui.Composables.BannerAd
+import com.example.pgdquiz.ui.Composables.ButtonGrid
+import com.example.pgdquiz.ui.Composables.Questionfield
 
 @Composable
-fun DrainLayout(){
-    Banner(examType = , examEmoji = , emojiCont = , attempts = )
+fun DrainLayout() {
+    Banner(
+        examType ="",
+        examEmoji = painterResource(R.drawable.happypoo),
+        emojiCont ="",
+        attempts ="")
+    Questionfield(
+        question ="",
+        icon = painterResource(R.drawable.flame1),
+        streakcount ="",
+        questionbackground = painterResource(R.drawable.buttonoff),
+        streak ="")
+    ButtonGrid()
+    BannerAd()
 
 }
