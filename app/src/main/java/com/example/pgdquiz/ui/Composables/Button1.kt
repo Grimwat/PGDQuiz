3ac8508dcd312489b78f292bf2bd4e5740702491
Thomas <Thomas.watson555@Gmail.com>
2024-10-08 06:17:22 +1300

    Button Grid

--- app/src/main/java/com/example/pgdquiz/ui/Composables/Button1.kt
+++ app/src/main/java/com/example/pgdquiz/ui/Composables/Button1.kt
@@ -1,60 +1,108 @@
 package com.example.pgdquiz.ui.Composables
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.StrokeCap.Companion.Square
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pgdquiz.R
+import com.example.pgdquiz.ui.theme.PgdQuizTheme
 
 
 @Composable
 fun AnswerButton(
     modifier: Modifier = Modifier,
-    drawableResourceId: Int,
-    questionResourceId: Int,
-    contentDescriptionId: Int,
+    drawableResourceId: Painter,
+    questionResourceId: String,
+    contentDescriptionId: String,
     onImageClick: () -> Unit
-){
-    Button(
-        onClick = onImageClick) {
-        Image(
-            painter = painterResource(drawableResourceId),
-            contentDescription = stringResource(contentDescriptionId))
-        Text(
-            text = stringResource(questionResourceId))
-        
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = onImageClick,
+            shape = RectangleShape,
+            modifier = Modifier
+                .background(Color.Transparent)
+        ) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .background(Color.Transparent)
+            ) {
+                Image(
+                    painter = drawableResourceId,
+                    contentDescription = contentDescriptionId,
+                    modifier = modifier
+                )
+                Text(
+                    text = questionResourceId
+                )
+            }
+        }
     }
 }
 
 @Composable
-fun ButtonGrid(){
-    Column {
-        Row {
+fun ButtonGrid(
+    modifier: Modifier = Modifier
+) {
+    val ButtonOff = painterResource(R.drawable.buttonoff)
+    var Questionset = "gg"
+    val ContentDes = "No Help here Cuz"
+    Column (){
+        Row (modifier = modifier){
             AnswerButton(
-                drawableResourceId =,
-                questionResourceId =,
-                contentDescriptionId =
+                drawableResourceId = ButtonOff,
+                questionResourceId = Questionset,
+                contentDescriptionId = ContentDes,
+                modifier = Modifier.weight(1f)
             ) {}
             AnswerButton(
-                drawableResourceId =,
-                questionResourceId =,
-                contentDescriptionId =
+                drawableResourceId = ButtonOff,
+                questionResourceId = Questionset,
+                contentDescriptionId = ContentDes,
+                modifier = Modifier.weight(1f)
             ) {}
         }
-        Row {
+        Row(modifier = Modifier) {
             AnswerButton(
-                drawableResourceId = ,
-                questionResourceId = ,
-                contentDescriptionId = ) {}
+                drawableResourceId = ButtonOff,
+                questionResourceId = Questionset,
+                contentDescriptionId = ContentDes,
+                modifier = Modifier.weight(1f)
+            ) {}
             AnswerButton(
-                drawableResourceId = ,
-                questionResourceId = ,
-                contentDescriptionId = ) {}
+                drawableResourceId = ButtonOff,
+                questionResourceId = Questionset,
+                contentDescriptionId = ContentDes,
+                modifier = Modifier.weight(1f)
+            ) {}
         }
     }
+}
+
+@Preview
+@Composable
+fun GreetingPreview() {
+    PgdQuizTheme {
+        ButtonGrid()
     }
+}
--- app/src/main/java/com/example/pgdquiz/ui/Composables/QuizScreen.kt
+++ app/src/main/java/com/example/pgdquiz/ui/Composables/QuizScreen.kt
@@ -3,10 +3,3 @@ package com.example.pgdquiz.ui.Composables
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 
-@Composable
-fun QuizScreen (){
-    Image(
-        painter = ,
-        contentDescription = )
-
-}
