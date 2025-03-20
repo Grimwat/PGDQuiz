18d8040310dd7932926536ee2b42462fabe1da8c
Thomas <Thomas.watson555@Gmail.com>
2025-03-20 16:47:33 +1300

    Drainlaying 3 Test Questions

--- app/src/main/java/com/example/pgdquiz/ui/Composables/Constants.kt
+++ app/src/main/java/com/example/pgdquiz/ui/Composables/Constants.kt
@@ -18,6 +18,29 @@ object Constants {
             2
         )
         questionsList.add(que1)
+
+        val que2 = Question(
+            2,
+            "A Section of drain acting as a vent must not be smaller than what size?",
+            "50mm",
+            "32mm",
+            "65mm",
+            "80mm",
+            "40mm",
+            3
+        )
+        questionsList.add(que2)
+        val que3 = Question(
+            3,
+            "What is the minimum permitted width of a trench excavated for a 150mm drain?",
+            "350mm",
+            "200mm",
+            "300mm",
+            "250mm",
+            "450mm",
+            1
+        )
+        questionsList.add(que3)
         return questionsList
     }
 }
