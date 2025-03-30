3b7a540fa32929720cde569755d14f5906904ad5
Thomas <Thomas.watson555@Gmail.com>
2025-03-31 12:01:34 +1300

    Question and Answers data updated

--- app/src/main/java/com/example/pgdquiz/ui/Questionaire.kt
+++ app/src/main/java/com/example/pgdquiz/ui/Questionaire.kt
@@ -3,10 +3,15 @@ package com.example.pgdquiz.ui
 data class Question (
     val id: Int,
     val question: String,
-    val optionOne: String,
-    val optionTwo: String,
-    val optionThree: String,
-    val optionFour: String,
-    val optionFive: String,
-    val correctAnswer: Int
-)
+    val correctAnswer: String,
+    val incorrectAnswers: List<String>
+){
+    val shuffledOptions: List<String>
+    val incorrectAnswerIndex: Int
+
+    init {
+        val allOptions = incorrectAnswers + correctAnswer
+        shuffledOptions = allOptions.shuffled()
+        incorrectAnswerIndex = shuffledOptions.indexOf(correctAnswer)
+    }
+}
