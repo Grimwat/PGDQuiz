bfa696fc688842041304ee554832acd24f4e09be
Thomas <Thomas.watson555@Gmail.com>
2025-03-31 12:32:48 +1300

    Next Button fixed

--- app/src/main/java/com/example/pgdquiz/ui/ui/NextButton.kt
+++ app/src/main/java/com/example/pgdquiz/ui/ui/NextButton.kt
@@ -2,16 +2,23 @@ package com.example.pgdquiz.ui.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.pgdquiz.R
 
 @Composable
@@ -28,21 +35,24 @@ fun NextButton (modifier: Modifier = Modifier) {
             modifier = Modifier
                 .background(Color.Transparent)
         )
-        {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .background(Color.Transparent)
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.nextbutton),
-                    contentDescription = "Next Question",
-                    modifier = modifier
+        {Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(8.dp))
+                .background(
+                    color = MaterialTheme.colorScheme.outline
+
                 )
-                Text(
-                    text = "Flush"
+                .border(
+                    width = 4.dp,
+                    color = MaterialTheme.colorScheme.primary
+
                 )
-            }
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Next Question", color = Color.White)
+        }
         }
     }
 }
