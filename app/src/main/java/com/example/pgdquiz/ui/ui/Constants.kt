7e320413c054c8a065d9ab0b2f94e33ef9386f75
Thomas <Thomas.watson555@Gmail.com>
2025-03-31 12:12:48 +1300

    Question Store and test Questions

--- app/src/main/java/com/example/pgdquiz/ui/ui/Constants.kt
+++ app/src/main/java/com/example/pgdquiz/ui/ui/Constants.kt
@@ -3,44 +3,29 @@ package com.example.pgdquiz.ui.ui
 import com.example.pgdquiz.ui.Question
 
 object Constants {
+    fun getQuestions(): List<Question> {
+        return listOf(
 
-    fun getQuestions(): ArrayList<Question> {
-        val questionsList = ArrayList<Question>()
+            Question(
+                id = 1,
+                question = "According to AS/NZS3500 Part 2: Sanitary plumbing and drainage, what is the minimum height an overflow relief gully trap grate is permitted to be installed above unpaved ground level?",
+                correctAnswer = "75mm",
+                incorrectAnswers = listOf("50mm", "100mm", "125mm", "150mm")
+            ),
+            Question(
+                id = 2,
+                question = "A section of drain acting as a vent must not be smaller than what size?",
+                correctAnswer = "65mm",
+                incorrectAnswers = listOf("50mm", "32mm", "80mm", "40mm")
+            ),
+            Question(
+                id = 3,
+                question = "What is the minimum permitted width of a trench excavated for a 150mm drain?",
+                correctAnswer = "350mm",
+                incorrectAnswers = listOf("300mm", "200mm", "250mm", "450mm")
+            )
+        )
 
-        val que1 = Question(
-            1,
-            "According to AS/NZS3500 Part 2: Sanitary plumbing and drainage, what is the minimum height an overflow relief gully trap grate is permitted to be installed above unpaved ground level?",
-            "50mm",
-            "75mm",
-            "100mm",
-            "125mm",
-            "150mm",
-            2
-        )
-        questionsList.add(que1)
-
-        val que2 = Question(
-            2,
-            "A Section of drain acting as a vent must not be smaller than what size?",
-            "50mm",
-            "32mm",
-            "65mm",
-            "80mm",
-            "40mm",
-            3
-        )
-        questionsList.add(que2)
-        val que3 = Question(
-            3,
-            "What is the minimum permitted width of a trench excavated for a 150mm drain?",
-            "350mm",
-            "200mm",
-            "300mm",
-            "250mm",
-            "450mm",
-            1
-        )
-        questionsList.add(que3)
-        return questionsList
     }
+}
 }
