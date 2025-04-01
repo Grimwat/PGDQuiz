204ae2df1fc7d7798d55587f6836f916e8605294
Thomas <Thomas.watson555@Gmail.com>
2025-04-02 12:02:28 +1300

    Banner Changes

--- app/src/main/java/com/example/pgdquiz/ui/ui/Banner.kt
+++ app/src/main/java/com/example/pgdquiz/ui/ui/Banner.kt
@@ -18,12 +18,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.pgdquiz.R
 import com.example.pgdquiz.ui.theme.PgdQuizTheme
 
 @Composable
@@ -31,10 +29,10 @@ fun Banner(
     examType: String,
     examEmoji: Painter,
     emojiCont: String,
-    attempts: String,
+    attempts: Int,
     icon: Painter,
     streak: String,
-    streakcount: String,
+    streakCount: Int,
     modifier: Modifier = Modifier
 ) {
         Box(
@@ -67,7 +65,7 @@ fun Banner(
                         .size(40.dp),
                 )
                 Text(
-                    text = streakcount,
+                    text = streakCount.toString(),
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier
                         .padding(top = 10.dp),
@@ -84,7 +82,7 @@ fun Banner(
                 )
 
                 Text(
-                    text = attempts,
+                    text = attempts.toString(),
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier
                         .padding(top = 10.dp),
@@ -101,14 +99,5 @@ fun Banner(
 @Composable
 fun Drainpreview() {
     PgdQuizTheme {
-        Banner(
-            examType = "Drainlaying",
-            examEmoji = painterResource(R.drawable.happypoo),
-            emojiCont = "happyPoo",
-            attempts = "3",
-            icon = painterResource(R.drawable.flame1),
-            streak = "Streak Count",
-            streakcount = "3"
-        )
     }
 }
