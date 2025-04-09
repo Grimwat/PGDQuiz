dd05bfb6918a6b61200cecb5fc453ead1f58c7e8
Thomas <Thomas.watson555@Gmail.com>
2025-04-09 14:22:50 +1200

    Congratsoverlay added

--- app/src/main/java/com/example/pgdquiz/ui/ui/CongratsOverlay.kt
+++ app/src/main/java/com/example/pgdquiz/ui/ui/CongratsOverlay.kt
@@ -1,2 +1,92 @@
 package com.example.pgdquiz.ui.ui
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CongratulationsScreen(
+    modifier: Modifier = Modifier,
+    examEmoji: Painter,
+    emojiCont: String,
+    onRestart: () -> Unit,
+    onBackToModeSelect: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.tertiary.copy(alpha = 0.9f)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                text = "Congratulations!",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Text(
+                text = "You've completed all the questions!",
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center
+            )
+            Image(
+                painter = examEmoji,
+                contentDescription = emojiCont
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Button(
+                    onClick = onRestart,
+                    shape = RoundedCornerShape(8.dp),
+                    border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.outline,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text("Restart")
+                }
+
+                Button(
+                    onClick = onBackToModeSelect,
+                    shape = RoundedCornerShape(8.dp),
+                    border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary.copy(alpha = 0.3f)),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.outline,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text("Back")
+                }
+            }
+        }
+    }
+}
