53909d7df33c0f73163abc77c39ae70e41451878
Thomas <Thomas.watson555@Gmail.com>
2025-04-14 15:50:39 +1200

    Plumbing and gas themes added

--- app/src/main/java/com/example/pgdquiz/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pgdquiz/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.pgdquiz.ui.theme
 
+import android.R.id.primary
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -10,25 +11,78 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import com.example.pgdquiz.ui.ui.QuizType
 
-private val DarkColorScheme = darkColorScheme(
+private val DrainDarkScheme = darkColorScheme(
     primary = DarkRed,
     secondary = Green,
     tertiary = Grey,
     background = Black,
     surface = DarkGrey,
     outline = LightRed,
-//    outlinetwo = DarkGrey
-
 )
-
-private val LightColorScheme = lightColorScheme(
+private val DrainLightScheme = lightColorScheme(
     primary = LightRed,
     secondary = Green,
     tertiary = Grey,
     background = Black,
     surface = DarkGrey,
     outline = DarkRed,
+)
+private val DarkColorScheme = darkColorScheme (
+    primary = LightRed,
+    secondary = Blue,
+    onSurface = DarkBLue,
+    onBackground = Yellow,
+    onPrimary = Gas,
+    tertiary = Grey,
+    background = Black,
+    surface = DarkGrey,
+    outline = DarkRed,
+)
+private val LightColorScheme = lightColorScheme(
+    primary = LightRed,
+    secondary = Blue,
+    onSurface = DarkBLue,
+    onBackground = Yellow,
+    onPrimary = Gas,
+    tertiary = Grey,
+    background = Black,
+    surface = DarkGrey,
+    outline = DarkRed,
+)
+private val GasfittingDarkColorScheme = darkColorScheme (
+    primary = Gas,
+    secondary = Green,
+    tertiary = Grey,
+    background = Black,
+    surface = DarkGrey,
+    outline = Yellow,
+    )
+private val GasfittingColorScheme = lightColorScheme(
+    primary = Yellow,
+    secondary = Green,
+    tertiary = Grey,
+    background = Black,
+    surface = DarkGrey,
+    outline = Gas,
+)
+private val PlumbingDarkColorScheme = darkColorScheme (
+    primary = DarkBLue,
+    secondary = Green,
+    tertiary = Grey,
+    background = Black,
+    surface = DarkGrey,
+    outline = Blue,
+    )
+private val PlumbingColorScheme = lightColorScheme(
+    primary = Blue,
+    secondary = Green,
+    tertiary = Grey,
+    background = Black,
+    surface = DarkGrey,
+    outline = DarkBLue,
+)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -39,12 +93,12 @@ private val LightColorScheme = lightColorScheme(
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
     */
-)
+
 
 @Composable
 fun PgdQuizTheme(
+    quizType: QuizType,
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
@@ -53,11 +107,15 @@ fun PgdQuizTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
+        quizType == QuizType.GASFITTING && darkTheme -> GasfittingDarkColorScheme
+        quizType == QuizType.GASFITTING -> GasfittingColorScheme
+        quizType == QuizType.PLUMBING && darkTheme -> PlumbingDarkColorScheme
+        quizType == QuizType.PLUMBING -> PlumbingColorScheme
+        quizType == QuizType.DRAINLAYING && darkTheme -> DrainDarkScheme
+        quizType == QuizType.DRAINLAYING -> DrainLightScheme
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
