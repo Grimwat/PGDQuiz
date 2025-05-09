663518f4322fc7711659cb7e2ed465aff5c3169b
Thomas <Thomas.watson555@Gmail.com>
2025-05-10 10:03:06 +1200

    Quizstate added

---
+++ app/src/main/java/com/example/pgdquiz/ui/ui/quizstate.kt
@@ -0,0 +1,10 @@
+package com.example.pgdquiz.ui.ui
+
+data class QuizState(
+    var questions: List<Question> = emptyList(),
+    var currentQuestionIndex: Int = 0,
+    var streakCount: Int = 0,
+    var lives: Int = 3,
+    var quizComplete: Boolean = false,
+    var selectedAnswers: MutableSet<String> = mutableSetOf()
+)
