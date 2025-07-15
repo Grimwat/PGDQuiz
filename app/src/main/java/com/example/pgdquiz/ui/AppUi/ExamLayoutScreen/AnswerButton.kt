a5c6e110459a7d2b1687993f3e3add0ee02fac8b
Thomas <Thomas.watson555@Gmail.com>
2025-07-16 06:26:42 +1200

    clean up

--- app/src/main/java/com/example/pgdquiz/ui/AppUi/ExamLayoutScreen/AnswerButton.kt
+++ app/src/main/java/com/example/pgdquiz/ui/AppUi/ExamLayoutScreen/AnswerButton.kt
@@ -1,2 +1,92 @@
 package com.example.pgdquiz.ui.AppUi.ExamLayoutScreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AnswerButton(
+    optionText: String,
+    isSelected: Boolean,
+    onButtonSelected: () -> Unit,
+    modifier: Modifier = Modifier,
+    isCorrect: Boolean,
+    showCorrectAnswer: Boolean
+) {
+    val backgroundBrush = when {
+        showCorrectAnswer && isCorrect -> Brush.radialGradient(
+            colors = listOf(MaterialTheme.colorScheme.secondary, MaterialTheme.colorScheme.secondary),
+            radius = 600f
+        )
+
+        isSelected -> Brush.radialGradient(
+            colors = listOf(MaterialTheme.colorScheme.outline, MaterialTheme.colorScheme.primary),
+            radius = 600f
+        )
+
+        else -> Brush.radialGradient(
+            colors = listOf(
+                MaterialTheme.colorScheme.primary,
+                MaterialTheme.colorScheme.background
+            ),
+            radius = 600f
+        )
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = onButtonSelected,
+            shape = RectangleShape,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Unspecified
+            ),
+            modifier = Modifier.background(Color.Transparent)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(brush = backgroundBrush)
+                    .border(
+                        width = 2.dp,
+                        color = if (isSelected) MaterialTheme.colorScheme.outline
+                        else MaterialTheme.colorScheme.primary,
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .padding(12.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = optionText,
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/pgdquiz/ui/AppUi/ExamLayoutScreen/TopBanner.kt
+++ app/src/main/java/com/example/pgdquiz/ui/AppUi/ExamLayoutScreen/TopBanner.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.pgdquiz.ui.QuizType
 
 @Composable
 fun Banner(
