87f3d7db00c14521c5bcca483b07b7ff48bfee5b
Thomas <Thomas.watson555@Gmail.com>
2025-08-16 08:44:13 +1200

    Import clean up

--- app/src/main/java/com/example/pgdquiz/ui/AppUi/MainScreen/QuizTypeButton.kt
+++ app/src/main/java/com/example/pgdquiz/ui/AppUi/MainScreen/QuizTypeButton.kt
@@ -1,7 +1,6 @@
 package com.example.pgdquiz.ui.AppUi.MainScreen
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -19,17 +18,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.pgdquiz.R
 import com.example.pgdquiz.ui.Data.QuizType
 import com.example.pgdquiz.ui.theme.BackgroundLogo
 
